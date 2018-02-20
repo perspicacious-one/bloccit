8b213e163a3860a15722cee6461e38d2b716c5e6
Nelson <nelsondcraig@gmail.com>
2018-02-19 16:26:15 -0800

    checkpoint-27 work done

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,9 +41,18 @@
       body: RandomData.random_paragraph
     )
   end
-  user = User.first
-  user.update_attributes!(
-    email: 'nelsondcraig@gmail.com',
+  # Create an admin user
+  admin = User.create!(
+    name:     'Admin User',
+    email:    'admin@example.com',
+    password: 'helloworld',
+    role:     'admin'
+  )
+
+  # Create a member
+  member = User.create!(
+    name:     'Member User',
+    email:    'member@example.com',
     password: 'helloworld'
   )
 
