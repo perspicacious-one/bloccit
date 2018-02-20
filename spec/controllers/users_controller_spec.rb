a317fcf9a73e8426a88cbc5f95c9e0ffbff6b664
Nelson <nelsondcraig@gmail.com>
2018-02-19 20:58:25 -0800

    checkpoint 28 work done

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -52,7 +52,7 @@ RSpec.describe UsersController, type: :controller do
       expect(assigns(:user).password_confirmation).to eq new_user_attributes[:password_confirmation]
     end
     it "logs the user in after sign up" do
-      post :create, params: { user: new_user.attributes }
+      post :create, params: { user: new_user_attributes }
       expect(session[:user_id]).to eq assigns(:user).id
     end
   end
